c7bb6abdf3c879e466a2a70e7af54aea89a94443
Aslak <51925794+AslakOffi@users.noreply.github.com>
2021-11-01 21:00:36 +0100

    Add files via upload

---
+++ Controls/MainMenuStrip.cs
@@ -0,0 +1,79 @@
+﻿using System.Windows.Forms;
+
+namespace LunaEditor.Controls
+{
+    public sealed class MainMenuStrip : MenuStrip
+    {
+        public MainMenuStrip()
+        {
+            Name = "MainMenuStrip";
+            Dock = DockStyle.Top;
+            
+            FileDropDownMenu();
+            EditDropDownMenu();
+            FormatDropDownMenu();
+            ViewDropDownMenu();
+        }
+
+        public void FileDropDownMenu()
+        {
+            var fileDropDownMenu = new ToolStripMenuItem("Fichier");
+
+            var newMenu = new ToolStripMenuItem("Nouveau", null, null, Keys.Control | Keys.N);
+            var openMenu = new ToolStripMenuItem("Ouvrir...", null, null, Keys.Control | Keys.O);
+            var saveMenu = new ToolStripMenuItem("Enregister", null, null, Keys.Control | Keys.S);
+            var saveAsMenu = new ToolStripMenuItem("Enregistrer sous...", null, null, Keys.Control | Keys.Shift | Keys.N);
+            var quitMenu = new ToolStripMenuItem("Quitter", null, null, Keys.Alt | Keys.F4);
+
+            fileDropDownMenu.DropDownItems.AddRange(new ToolStripItem[] {newMenu, openMenu, saveMenu, saveAsMenu, quitMenu});
+
+            Items.Add(fileDropDownMenu);
+        }
+
+        public void EditDropDownMenu()
+        {
+            var editDropDownMenu = new ToolStripMenuItem("Edition");
+
+            var cancelMenu = new ToolStripMenuItem("Annuler", null, null, Keys.Control | Keys.Z);
+            var restoreMenu = new ToolStripMenuItem("Restaurer", null, null, Keys.Control | Keys.Y);
+
+            editDropDownMenu.DropDownItems.AddRange(new ToolStripItem[] {cancelMenu, restoreMenu});
+
+            Items.Add(editDropDownMenu);
+        }
+
+        public void FormatDropDownMenu()
+        {
+            var formatDropDownMenu = new ToolStripMenuItem("Format");
+
+            var fontMenu = new ToolStripMenuItem("Police...");
+
+            formatDropDownMenu.DropDownItems.AddRange(new ToolStripItem[] { fontMenu });
+
+            Items.Add(formatDropDownMenu);
+        }
+
+        public void ViewDropDownMenu()
+        {
+            var viewDropDownMenu = new ToolStripMenuItem("Affichage");
+
+            var alwaysOnTopMenu = new ToolStripMenuItem("Toujours devant");
+            
+            var zoomDropDownMenu = new ToolStripMenuItem("Zoom");
+            
+            var zoomInMenu = new ToolStripMenuItem("Zoom avant", null, null, Keys.Control | Keys.Add);
+            var zoomOutMenu = new ToolStripMenuItem("Zoom arrière", null, null, Keys.Control | Keys.Subtract);
+            var zoomResetMenu = new ToolStripMenuItem("Restaurer le zoom par défaut", null, null, Keys.Control | Keys.Divide);
+
+            zoomInMenu.ShortcutKeyDisplayString = "Ctrl+Num +";
+            zoomOutMenu.ShortcutKeyDisplayString = "Ctrl+Num -";
+            zoomResetMenu.ShortcutKeyDisplayString = "Ctrl+Num /";
+
+            zoomDropDownMenu.DropDownItems.AddRange(new ToolStripItem[] { zoomInMenu, zoomOutMenu, zoomResetMenu });
+            
+            viewDropDownMenu.DropDownItems.AddRange(new ToolStripItem[] { alwaysOnTopMenu, zoomDropDownMenu });
+
+            Items.Add(viewDropDownMenu);
+        }
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LunaEditor.Controls;
+
+namespace LunaEditor
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+      MainMenuStrip menuStrip = new MainMenuStrip();
+      Controls.Add(menuStrip);
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
